89659c9c88bfdc5715f8225bdab0351e53aa0e09
Trần Sỹ cao <honghoang1232@gmail.com>
2019-05-28 00:45:04 +0700

    like news

--- src/main/java/com/bksoftware/controller/viewer/news/LikeNewsController.java
+++ src/main/java/com/bksoftware/controller/viewer/news/LikeNewsController.java
@@ -7,10 +7,7 @@ import com.bksoftware.service.news.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -38,17 +35,27 @@ public class LikeNewsController {
         News news = newsService.findById(idNews);
         boolean result = likeNewsService.findLikeNew(idNews, idUser);
         if (result) {
-            news.setLike(news.getLike() + 1);
-            newsService.saveNews(news);
-            return new ResponseEntity<>("find by user like news", HttpStatus.OK);
-        } else {
-            LikeNews likeNews = new LikeNews();
-            likeNews.setIdNews(idNews);
-            likeNews.setIdUser(idUser);
-            likeNewsService.saveLikeNews(likeNews);
-            return new ResponseEntity<>("find by user like news", HttpStatus.OK);
+            return new ResponseEntity<>("1", HttpStatus.OK);
         }
+        return new ResponseEntity<>("0",HttpStatus.OK);
+    }
 
+    @PostMapping
+    public ResponseEntity<Object> saveLikeByUserAndNews(
+            @RequestParam("id-user") int idUser,
+            @RequestParam("id-news") int idNews,
+            HttpServletResponse response
+
+    ) {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        News news = newsService.findById(idNews);
+        news.setLike(news.getLike() + 1);
+        newsService.saveNews(news);
+        LikeNews likeNews = new LikeNews();
+        likeNews.setIdNews(idNews);
+        likeNews.setIdUser(idUser);
+        likeNewsService.saveLikeNews(likeNews);
+        return new ResponseEntity<>("1", HttpStatus.OK);
 
     }
 }
